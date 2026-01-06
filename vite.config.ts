3547c538c57222edb0d0be3d6a442db800db54d0
thejahangir <jahangirxl@gmail.com>
2026-01-07 03:36:46 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,3 @@
-// import path from 'path';
-// import { defineConfig, loadEnv } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig(({ mode }) => {
-  
-//     const env = loadEnv(mode, '.', '');
-//     return {
-//       base: "/Antern-Final-Codebase/",
-//       server: {
-//         port: 3000,
-//         host: '0.0.0.0',
-//       },
-//       plugins: [react()],
-//       resolve: {
-//         alias: {
-//           '@': path.resolve(__dirname, '.'),
-//         }
-//       }
-//     };
-// });
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
